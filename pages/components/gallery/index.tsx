0ec9b24dc5fa3d0359a282737f3859129b1f70d1
ssk-dev-3 <89833989+ssk-dev-3@users.noreply.github.com>
2023-01-27 06:59:40 +0900

    ✨背景コンポーネントを追加

--- pages/components/gallery/index.tsx
+++ pages/components/gallery/index.tsx
@@ -3,6 +3,7 @@ import Button001 from '../items/button001';
 import Loader001 from '../items/loader001';
 import Loader002 from '../items/loader002';
 import Background001 from '../items/background001';
+import Background002 from '../items/background002';
 import Card001 from '../items/card001';
 import galleryStyle from './gallery.module.css';
 
@@ -24,6 +25,9 @@ const Gallery: React.FC = () => {
         </Card>
         <Card category='card'>
           <Card001 />
+        </Card>
+        <Card category='background'>
+          <Background002 />
         </Card>
       </div>
     </div>
---
+++ pages/components/items/background002/index.tsx
@@ -0,0 +1,9 @@
+import backgroundStyle from './background002.module.css';
+
+const Background002 = () => {
+  return (
+    <div className={backgroundStyle.checkerboard}></div>
+  );
+}
+
+export default Background002;
